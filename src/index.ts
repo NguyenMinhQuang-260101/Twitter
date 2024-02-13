cc8daec55221f4787ef58b0da8aab1edb86fe8d6
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-02-13 13:46:54 +0700

    Connect Mongo Atlas by MongoDB Driver

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const name: string = 'Nguyễn Minh Quang'
-console.log(name)
+import express from 'express'
+import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
+
+const app = express()
+const port = 3000
+app.use(express.json())
+app.use('/users', usersRouter)
+
+databaseService.connect()
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
