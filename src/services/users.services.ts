052f6faa254b5ee7cc46780d8d00c6cfb35443a7
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-02-14 14:54:08 +0700

    Update logic login and config env

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,9 +1,13 @@
+import { config } from 'dotenv'
+import { ObjectId } from 'mongodb'
 import { TokenType } from '~/constants/enums'
 import { RegisterReqBody } from '~/models/requests/User.requests'
+import RefreshToken from '~/models/schemas/RefreshToken.schema'
 import User from '~/models/schemas/User.schema'
 import databaseService from '~/services/database.services'
 import { hashPassword } from '~/utils/crypto'
 import { signToken } from '~/utils/jwt'
+config()
 
 class UsersService {
   private signAccessToken(userId: string) {
@@ -45,8 +49,16 @@ class UsersService {
 
     const user_id = result.insertedId.toString()
     const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
-
-    return { access_token, refresh_token }
+    await databaseService.refreshTokens.insertOne(
+      new RefreshToken({
+        user_id: new ObjectId(user_id),
+        token: refresh_token
+      })
+    )
+    return {
+      access_token,
+      refresh_token
+    }
   }
 
   async checkEmailExist(email: string) {
@@ -56,7 +68,16 @@ class UsersService {
 
   async login(user_id: string) {
     const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
-    return { access_token, refresh_token }
+    await databaseService.refreshTokens.insertOne(
+      new RefreshToken({
+        user_id: new ObjectId(user_id),
+        token: refresh_token
+      })
+    )
+    return {
+      access_token,
+      refresh_token
+    }
   }
 }
 
