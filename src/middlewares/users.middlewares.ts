2e11c037a7730c789be41d8cb0e2ee44d42b5b60
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-02-14 14:15:14 +0700

    Handling Login

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,19 +1,52 @@
 import { validate } from '~/utils/validation'
-import { Request, Response, NextFunction } from 'express'
 import { checkSchema } from 'express-validator'
 import usersService from '~/services/users.services'
-import { ErrorWithStatus } from '~/models/Errors'
 import { USERS_MESSAGES } from '~/constants/message'
+import databaseService from '~/services/database.services'
 
-export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
-  const { email, password } = req.body
-  if (!email || !password) {
-    return res.status(400).json({
-      error: 'Missing email or password'
-    })
-  }
-  next()
-}
+export const loginValidator = validate(
+  checkSchema({
+    email: {
+      isEmail: { errorMessage: USERS_MESSAGES.EMAIL_IS_INVALID },
+      trim: true,
+      custom: {
+        options: async (value, { req }) => {
+          const user = await databaseService.users.findOne({ email: value })
+          if (user === null) {
+            throw new Error(USERS_MESSAGES.USER_NOT_FOUND)
+          }
+          req.user = user
+          return true
+        }
+      }
+    },
+
+    password: {
+      notEmpty: { errorMessage: USERS_MESSAGES.PASSWORD_IS_REQUIRED },
+      isString: {
+        errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_A_STRING
+      },
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        },
+        errorMessage: USERS_MESSAGES.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+      },
+
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRONG
+      }
+    }
+  })
+)
 
 export const registerValidator = validate(
   checkSchema({
@@ -33,6 +66,7 @@ export const registerValidator = validate(
       },
       trim: true
     },
+
     email: {
       notEmpty: {
         errorMessage: USERS_MESSAGES.EMAIL_IS_REQUIRED
@@ -49,6 +83,7 @@ export const registerValidator = validate(
         }
       }
     },
+
     password: {
       notEmpty: { errorMessage: USERS_MESSAGES.PASSWORD_IS_REQUIRED },
       isString: {
@@ -61,6 +96,7 @@ export const registerValidator = validate(
         },
         errorMessage: USERS_MESSAGES.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
       },
+
       isStrongPassword: {
         options: {
           minLength: 6,
@@ -72,6 +108,7 @@ export const registerValidator = validate(
         errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRONG
       }
     },
+
     confirm_password: {
       notEmpty: { errorMessage: USERS_MESSAGES.CONFIRM_PASSWORD_IS_REQUIRED },
       isString: {
@@ -103,6 +140,7 @@ export const registerValidator = validate(
         }
       }
     },
+
     date_of_birth: {
       isISO8601: {
         options: {
