f616df1c723ccf5fc902b55c43b7d381135ea54b
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-02-17 13:37:51 +0700

    Code Function Upload Video

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,11 +1,19 @@
 import { Request, Response, NextFunction } from 'express'
 import path from 'path'
-import { UPLOAD_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/message'
 import mediasService from '~/services/medias.services'
 
 export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const url = await mediasService.handleUploadImage(req)
+  const url = await mediasService.uploadImage(req)
+  return res.json({
+    message: USERS_MESSAGES.UPLOAD_SUCCESS,
+    result: url
+  })
+}
+
+export const uploadVideoController = async (req: Request, res: Response, next: NextFunction) => {
+  const url = await mediasService.uploadVideo(req)
   return res.json({
     message: USERS_MESSAGES.UPLOAD_SUCCESS,
     result: url
@@ -15,9 +23,14 @@ export const uploadImageController = async (req: Request, res: Response, next: N
 export const serveImageController = (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.params
   const isNameJpg = !name.includes('.') ? name + '.jpg' : name
-  return res.sendFile(path.resolve(UPLOAD_DIR, isNameJpg), (err) => {
+  return res.sendFile(path.resolve(UPLOAD_IMAGE_DIR, isNameJpg), (err) => {
     if (err) {
       res.status((err as any).status).send('Not found')
     }
   })
+}
+
+export const serveVideoController = (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  return res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, name))
 }
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { defaultErrorHandle } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
 import { config } from 'dotenv'
-import { UPLOAD_DIR } from './constants/dir'
+import { UPLOAD_IMAGE_DIR } from './constants/dir'
 import staticRouter from './routes/static.routes'
 
 config()
@@ -20,7 +20,7 @@ app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
 app.use(defaultErrorHandle)
 app.use('/static', staticRouter)
-// app.use('/static', express.static(UPLOAD_DIR))
+// app.use('/static', express.static(UPLOAD_IMAGE_DIR))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
