4199fe1925bb2085fa7d214d6976a3fd4111d3c8
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-02-17 07:10:33 +0700

    Code Function Upload single image

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import express from 'express'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorHandle } from './middlewares/error.middlewares'
+import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
 databaseService.connect()
 const app = express()
 const port = 4000
+
+// Tạo folder upload
+initFolder()
+
 app.use(express.json())
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 app.use(defaultErrorHandle)
 
 app.listen(port, () => {
