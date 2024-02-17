3dfb29b3fe347ae6046fd17d669a9920c879c1d1
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-02-17 09:44:32 +0700

    Code Function Multi Image: - Switch code Function Upload Single Image to Multi Image - Fix code for Multi image

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -4,8 +4,8 @@ import { UPLOAD_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/message'
 import mediasService from '~/services/medias.services'
 
-export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const url = await mediasService.handleUploadSingleImage(req)
+export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
+  const url = await mediasService.handleUploadImage(req)
   return res.json({
     message: USERS_MESSAGES.UPLOAD_SUCCESS,
     result: url
@@ -14,7 +14,7 @@ export const uploadSingleImageController = async (req: Request, res: Response, n
 
 export const serveImageController = (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.params
-  const isNameJpg = !name.includes('.jpg') ? name + '.jpg' : name
+  const isNameJpg = !name.includes('.') ? name + '.jpg' : name
   return res.sendFile(path.resolve(UPLOAD_DIR, isNameJpg), (err) => {
     if (err) {
       res.status((err as any).status).send('Not found')
