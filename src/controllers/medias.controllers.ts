fbd0d9476ccd7b485fed093142078c588b06c306
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-02-18 16:32:13 +0700

    Update Functions: - Fix Serve Video Streaming - Add play HLS stream on client - Add check status video encode

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -31,10 +31,39 @@ export const uploadVideoHLSController = async (req: Request, res: Response, next
   })
 }
 
+export const videoStatusController = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params
+  const result = await mediasService.getVideoStatus(id as string)
+  return res.json({
+    message: USERS_MESSAGES.GET_VIDEO_STATUS_SUCCESS,
+    result: result
+  })
+}
+
 export const serveImageController = (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.params
   const isNameJpg = !name.includes('.') ? name + '.jpg' : name
   return res.sendFile(path.resolve(UPLOAD_IMAGE_DIR, isNameJpg), (err) => {
+    if (err) {
+      res.status((err as any).status).send('Not found')
+    }
+  })
+}
+
+export const serveM3u8Controller = (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params
+  return res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, id, 'master.m3u8'), (err) => {
+    if (err) {
+      res.status((err as any).status).send('Not found')
+    }
+  })
+}
+
+export const serveSegmentController = (req: Request, res: Response, next: NextFunction) => {
+  const { id, v, segment } = req.params
+  // Segment: 0.ts, 1.ts, 2.ts, ...
+  console.log(segment)
+  return res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, id, v, segment), (err) => {
     if (err) {
       res.status((err as any).status).send('Not found')
     }
@@ -46,8 +75,8 @@ export const serveVideoStreamController = (req: Request, res: Response, next: Ne
   if (!range) {
     return res.status(HTTP_STATUS.BAD_REQUEST).send('Requires Range header')
   }
-  const { name } = req.params
-  const videoPath = path.resolve(UPLOAD_VIDEO_DIR, name)
+  const { pathname, name } = req.params
+  const videoPath = path.resolve(UPLOAD_VIDEO_DIR + `\\${pathname}`, name)
   // 1MB = 10^6 bytes (Tính theo hệ 10, đây là thứ mà chúng ta hay thấy trên UI)
   // Còn nếu tính theo hệ nhị phân thì 1MB = 2^20 bytes (1024 * 1024)
 
