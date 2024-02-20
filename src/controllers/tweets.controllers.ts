4468b340123333d72f1605f3368ac78a9235d057
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-02-20 13:17:54 +0700

    Update Function Tweet: - Increase View when get Tweet - Get comments, retweet, quotes with pagination

--- src/controllers/tweets.controllers.ts
+++ src/controllers/tweets.controllers.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express'
 import { TweetRequestBody } from '~/models/requests/Tweet.requests'
 import tweetsService from '~/services/tweets.services'
 import { TokenPayload } from '~/models/requests/User.requests'
+import { TweetType } from '~/constants/enums'
 
 export const createTweetController = async (req: Request<ParamsDictionary, any, TweetRequestBody>, res: Response) => {
   const { user_id } = req.decoded_authorization as TokenPayload
@@ -14,10 +15,38 @@ export const createTweetController = async (req: Request<ParamsDictionary, any, 
 }
 
 export const getTweetDetailController = async (req: Request, res: Response) => {
-  // const { tweet_id } = req.params
-  // const result = await tweetsService.getTweetDetail(tweet_id)
+  const result = await tweetsService.increaseView(req.params.tweet_id, req.decoded_authorization?.user_id)
+  const tweet = {
+    ...req.tweet,
+    guest_views: result.guest_views,
+    user_views: result.user_views
+  }
   return res.json({
-    message: 'Get tweet detail successfully',
-    result: 'ok'
+    message: 'Get Tweet detail successfully',
+    result: tweet
+  })
+}
+
+export const getTweetChildrenController = async (req: Request, res: Response) => {
+  const tweet_type = Number(req.query.tweet_type as string) as TweetType
+  const limit = Number(req.query.limit as string)
+  const page = Number(req.query.page as string)
+
+  const { total, tweets } = await tweetsService.getTweetChildren({
+    tweet_id: req.params.tweet_id,
+    tweet_type,
+    limit,
+    page
+  })
+
+  return res.json({
+    message: 'Get Tweet Children successfully',
+    result: {
+      tweets,
+      tweet_type,
+      limit,
+      page,
+      total_pages: Math.ceil(total / limit)
+    }
   })
 }
--- src/routes/tweets.routes.ts
+++ src/routes/tweets.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
-import { createTweetController, getTweetDetailController } from '~/controllers/tweets.controllers'
+import {
+  createTweetController,
+  getTweetChildrenController,
+  getTweetDetailController
+} from '~/controllers/tweets.controllers'
 import { audienceValidator, createTweetValidator, tweetIdValidator } from '~/middlewares/tweets.middlewares'
 import { accessTokenValidator, isUserLoggedInValidator, verifiedUserValidator } from '~/middlewares/users.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers'
@@ -34,6 +38,22 @@ tweetsRouter.get(
   isUserLoggedInValidator(verifiedUserValidator),
   audienceValidator,
   wrapRequestHandler(getTweetDetailController)
+)
+
+/**
+ * Description: Get Tweet Children
+ * Path: /:tweet_id/children
+ * Method: GET
+ * Header: {Authorization?: Bearer <access_token>}
+ * Query: {limit: number, page: number, tweet_type: TweetType}
+ */
+tweetsRouter.get(
+  '/:tweet_id/children',
+  tweetIdValidator,
+  isUserLoggedInValidator(accessTokenValidator),
+  isUserLoggedInValidator(verifiedUserValidator),
+  audienceValidator,
+  wrapRequestHandler(getTweetChildrenController)
 )
 
 export default tweetsRouter
